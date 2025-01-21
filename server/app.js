5558416284fdc086cd7a706322b743b7f89d442e
Tai Nguyen <tainguyen@Tais-MacBook-Air-2.local>
2025-01-21 09:55:49 -0800

    initialized server side for app

---
+++ server/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+ const app = express();
+
+ app.get('/', (res, req) => {
+    // some code 
+ });
+
+ app.listen(3000, () => {
+    // running on local host for now
+ });
