3f17b65d112d7b491e7cff1e27eb017dae0c5e01
Tai Nguyen <tainguyen@Tais-MacBook-Air-2.local>
2025-01-21 21:33:38 -0800

    routed app, added components/structure

---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export function Header(){
+    <p>this is footer</p>
+}
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export function Home(){
+    return (
+        <p>Testing 1</p>
+    )
+}
