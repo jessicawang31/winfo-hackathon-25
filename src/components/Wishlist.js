6c3a06ea8b2b1737a9460c8d1d36fe510134ef6f
Jessica Wang <jessicaw.040131@gmail.com>
2025-01-24 20:43:45 -0800

    created wishlist

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,6 +1,42 @@
+// import React from 'react';
+// export function Wishlist(){
+//     return (
+//         <p>Testing Wishlist</p>
+//     )
+// }
+
 import React from 'react';
-export function Wishlist(){
+import '../css/Wishlist.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link } from 'react-router-dom';
+
+export function Wishlist() {
     return (
-        <p>Testing Wishlist</p>
-    )
+        <div className="wishlist-container">
+            <h2 className="wishlist-header">My Wishlist</h2>
+            <div className="wishlist-grid">
+                <Link to="/explore" className="wishlist-card">
+                    <div className="wishlist-card-icon">
+                        <i className="fa-solid fa-heart wishlist-icon"></i>
+                    </div>
+                    <div className="wishlist-card-tag purchased">Purchased</div>
+                </Link>
+                <Link to="/explore" className="wishlist-card">
+                    <div className="wishlist-card-icon">
+                        <i className="fa-solid fa-heart wishlist-icon"></i>
+                    </div>
+                </Link>
+                <Link to="/explore" className="wishlist-card">
+                    <div className="wishlist-card-icon">
+                        <i className="fa-solid fa-heart wishlist-icon"></i>
+                    </div>
+                </Link>
+                <Link to="/explore" className="wishlist-card">
+                    <div className="wishlist-card-icon">
+                        <i className="fa-solid fa-heart wishlist-icon"></i>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
 }
